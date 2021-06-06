0b7478b8f0492fb21343269ae7bd1640c8898e14
golubvladimir <golubvlauri@gmail.com>
2021-06-06 11:58:19 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useStore, computed } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+
+    const example = computed(() => store.state.example.count);
+
+    console.log({book});
+  }
+})
+</script>
+
+<style>
+
+</style>
